c7043725dcb92c2d3e1752973d83f2d0c63d2105
Ayush Patel <ayushap05@gmail.com>
2024-09-16 13:33:22 +0530

    Redux Slice for videoCall

--- chat-frontend/src/Components/VideoPage.jsx
+++ chat-frontend/src/Components/VideoPage.jsx
@@ -1,6 +1,8 @@
 import { useEffect, useRef, useState } from "react";
 import { useOutletContext } from "react-router-dom";
-import { useSelector } from "react-redux";
+// import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
+import { clearMediaStreams, setLocalMediaStream, setRemoteMediaStream } from "../slices/videoCallSlice";
 // import 
 
 const pcConstraints = {
@@ -22,29 +24,28 @@ const servers = {
 };
 
 const VideoPage = () => {
+  const dispatch = useDispatch();
+  const {localMediaStream, remoteMediaStream} = useSelector((state) => state.videoCall);
   const conversationId = useSelector((state) => state.chat.selectedConversationID);
   const userId = useSelector((state) => state.auth.user.id);
-  console.log("The conversation id is: ", conversationId);
   const [socket] = useOutletContext();
+  
   const localStream = useRef(null);
   const remoteStream = useRef(null);
+  const [callStatus, setCallStatus] = useState(false);
   const [peerConnection, setPeerConnection] = useState(null);
-  // const localPeerConnection = useRef(null);
   const [error, setError] = useState(null);
-  // const [answerButton, setAnswerButton] = useState(false);
-  // const peerConnection = new RTCPeerConnection(servers, pcConstraints);
-//   const [localStream, setLocalStream] = useState(null);
-//   const [remoteStream, setRemoteStream] = useState(null);
   useEffect(() => {
     const pc = new RTCPeerConnection(servers, pcConstraints);
     setPeerConnection(pc);
 
     function handleUnload() {
       console.log("Handel Unload fired");
-      if (localStream.current && localStream.current.srcObject) {
-        console.log("Cleaning localStream");
-        localStream.current.srcObject.getTracks().forEach((track) => track.stop());
-      }
+      // if (localStream.current && localStream.current.srcObject) {
+      //   console.log("Cleaning localStream");
+      //   localStream.current.srcObject.getTracks().forEach((track) => track.stop());
+      // }
+      dispatch(clearMediaStreams);
       peerConnection && peerConnection.close();
       console.log("PeerClosed");
     }
@@ -58,6 +59,16 @@ const VideoPage = () => {
       // window.removeEventListener('popstate', handleUnload);
     }
   }, []);
+
+
+  useEffect(() => {
+    if (remoteStream.current && remoteMediaStream) {
+      remoteStream.current.srcObject = remoteMediaStream;
+    }
+    if(localStream.current && localMediaStream) {
+      localStream.current.srcObject = localMediaStream;
+    }
+  }, [remoteMediaStream, localMediaStream]);
 
 
   useEffect(() => {
@@ -77,7 +88,9 @@ const VideoPage = () => {
         // console.log("OnTrack running: ",event);
         const stream = event.streams[0];
         // const remoteVideoElement = document.getElementById('remoteVideo');
-        remoteStream.current.srcObject = stream;
+        // remoteStream.current.srcObject = stream;
+        dispatch(setRemoteMediaStream(stream))
+        // remoteMediaStream = stream;
       }
     }
 
@@ -124,7 +137,9 @@ const VideoPage = () => {
           video: true,
           audio: true,
         });
-        localStream.current.srcObject = stream;
+        dispatch(setLocalMediaStream(stream));
+        // dispatch(clearMediaStreams());
+        // localStream.current.srcObject = stream;
       } catch (err) {
         setError("Failed to access media devices: " + err.message);
       }
@@ -139,25 +154,28 @@ const VideoPage = () => {
 
 
 
-
-
-  // When user clicks call button, we will create the p2p connection with RTCPeerConnection
-  const startLocalStream = async () => {
-    // console.log("Video Call attempt started");
+  const handelStartVideoCall = async () => {
     localStream.current.srcObject.getTracks().forEach((track) => {
       peerConnection.addTrack(track, localStream.current.srcObject);
     });
-
+    
     const offer = await peerConnection.createOffer();
-    // console.log("Offer created from frontend: ", offer);
-    // console.log("Offer sdp is: ", offer.sdp);
     await peerConnection.setLocalDescription(offer);
+    await setCallStatus(true);
     socket.emit("offer", {sdp: offer.sdp, conversationId});
   };
-
-
-
-
+  
+  
+  
+  
+  
+  function handelStopVideoCall() {
+    peerConnection.close();
+    setCallStatus(false);
+    dispatch(clearMediaStreams());
+  }
+  
+  
   return (
     <div className="flex flex-col items-center justify-center min-h-screen grow bg-gray-100">
   {error ? (
@@ -184,13 +202,19 @@ const VideoPage = () => {
           />
         </div>
       </div>
-      <button
-        onClick={startLocalStream}
-        className="mt-4 px-6 py-2 bg-blue-500 text-white rounded-lg hover:bg-blue-600"
-      >
-        Start
-      </button>
-      {/* <button onClick={onAnswer} className="mt-2 px-6 py-2 bg-green-500 text-white rounded-lg hover:bg-green-600">Accept</button> */}
+      {callStatus
+      ? <button
+          onClick={handelStopVideoCall}
+          className="mt-4 px-6 py-2 bg-blue-500 text-white rounded-lg hover:bg-blue-600">
+          Stop
+        </button>
+      : <button
+          onClick={handelStartVideoCall}
+          className="mt-4 px-6 py-2 bg-blue-500 text-white rounded-lg hover:bg-blue-600">
+          Start
+        </button>}
+        
+
     </>
   )}
 </div>
--- chat-frontend/src/slices/videoCallSlice.js
+++ chat-frontend/src/slices/videoCallSlice.js
@@ -9,18 +9,29 @@ const videoCallSlice = createSlice({
   name: "videoCall",
   initialState: initialState,
   reducers: {
-    // Action to set local media stream
     setLocalMediaStream(state, action) {
+        console.log("Setting media stream: ",action.payload);
       state.localMediaStream = action.payload;
     },
-    // Action to set remote media stream
     setRemoteMediaStream(state, action) {
       state.remoteMediaStream = action.payload;
     },
-    // Action to clear both media streams
     clearMediaStreams(state) {
-      state.localMediaStream = null;
-      state.remoteMediaStream = null;
+      // Stop all tracks of the local media stream
+      console.log("Here in media stream");
+      if (state.localMediaStream) {
+        console.log("Here is localMediaStream");
+        console.log(state.localMediaStream);
+        console.log(state.localMediaStream.getTracks())
+        state.localMediaStream.getTracks().forEach(track => track.stop());
+        state.localMediaStream = null;
+      }
+
+      // Stop all tracks of the remote media stream
+      if (state.remoteMediaStream) {
+        state.remoteMediaStream.getTracks().forEach(track => track.stop());
+        state.remoteMediaStream = null;
+      }
     },
   },
 });
